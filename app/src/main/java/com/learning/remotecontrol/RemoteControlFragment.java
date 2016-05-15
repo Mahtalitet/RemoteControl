caad6c174a922d1a9f9adcfaffad2af779c030b2
ilya <MAAFI4I@gmail.com>
2016-05-15 17:19:58 +0200

    Changed a layout of fragment

--- app/src/main/java/com/learning/remotecontrol/RemoteControlFragment.java
+++ app/src/main/java/com/learning/remotecontrol/RemoteControlFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,8 +21,6 @@ public class RemoteControlFragment extends Fragment {
 
     private TextView mSelectedTextView;
     private TextView mWorkingTextView;
-    private Button mZeroButton;
-    private Button mOneButton;
     private Button mEnterButton;
 
     public RemoteControlFragment() {
@@ -63,19 +63,42 @@ public class RemoteControlFragment extends Fragment {
                 if (working.equals("0")) {
                     mWorkingTextView.setText(text);
                 } else {
-                    mWorkingTextView.setText(text + working);
+                    mWorkingTextView.setText(working + text);
                 }
 
             }
         };
 
+        TableLayout tableLayout = (TableLayout) view.findViewById(R.id.fragment_remote_control_tableLayout);
 
-        mZeroButton = (Button) view.findViewById(R.id.fragment_remote_control_zeroButton);
-        mZeroButton.setOnClickListener(numberButtonListener);
+        int number = 1;
+        for (int i = 2; i < tableLayout.getChildCount() - 1; i++) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+            for (int j = 0; j < tableRow.getChildCount(); j++) {
+                Button button = (Button)tableRow.getChildAt(j);
+                button.setText(String.valueOf(number));
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+        }
 
-        mOneButton = (Button) view.findViewById(R.id.fragment_remote_control_oneButton);
-        mOneButton.setOnClickListener(numberButtonListener);
-        mEnterButton = (Button) view.findViewById(R.id.fragment_remote_control_enterButton);
+        TableRow bottomRow = (TableRow) tableLayout.getChildAt(tableLayout.getChildCount() - 1);
+
+        Button deleteButton = (Button) bottomRow.getChildAt(0);
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWorkingTextView.setText("0");
+            }
+        });
+
+        Button zeroButton = (Button) bottomRow.getChildAt(1);
+        zeroButton.setText("0");
+        zeroButton.setOnClickListener(numberButtonListener);
+
+        mEnterButton = (Button) bottomRow.getChildAt(2);
+        mEnterButton.setText("Enter");
         mEnterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
